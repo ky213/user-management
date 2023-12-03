60e2eb0d5f538d5d149fb4dc729854a52375c1df
YAAQOUB KEDDAOUI <y.keddaoui@gmail.com>
2023-12-03 22:06:51 +0100

    page navigation test suite

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -1,10 +1,17 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test("Page navigation", async ({ page }) => {
   await page.goto("http://localhost:3000");
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("User Management");
+
+  await page.goto("http://localhost:3000/dashboard");
+  await expect(page.getByText("Dashboard")).toBeVisible();
+
+  await page.getByRole("row").first().click();
+  await expect(page.getByText("Phone")).toBeVisible();
+
+  await page.goto("http://localhost:3000/dashboard/new-user");
+  await expect(page.getByText("Create new user")).toBeVisible();
 });
 
 test("get started link", async ({ page }) => {
